c563160e553153140dfa6f6959dfcbc0ce48e7d7
Mr.xia <love_mobile@163.com>
2013-11-29 01:24:08 +0800

    second test

---
+++ Classes/VideoPlayer.h
@@ -0,0 +1,13 @@
+//
+//  VideoPlayer.h
+//  GamePlayer
+//
+//  Created by talent_xia on 13-11-29.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VideoPlayer : NSObject
+
+@end
---
+++ Classes/VideoPlayer.m
@@ -0,0 +1,13 @@
+//
+//  VideoPlayer.m
+//  GamePlayer
+//
+//  Created by talent_xia on 13-11-29.
+//
+//
+
+#import "VideoPlayer.h"
+
+@implementation VideoPlayer
+
+@end
